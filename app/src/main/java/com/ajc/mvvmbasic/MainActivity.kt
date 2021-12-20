3aa0da6cb33288abf9f588630361dbb1590cfa26
Avadhutjc <Avadhutjc@gmail.com>
2021-12-20 19:55:42 +0530

    First

---
+++ app/src/main/java/com/ajc/mvvmbasic/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ajc.mvvmbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+echo "# MVVM-Basics" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/avadhutjc/MVVM-Basics.git
+git push -u origin main
+
+git remote add origin https://github.com/avadhutjc/MVVM-Basics.git
+git add *
+git commit -m "first commit"
+git push -u origin main
